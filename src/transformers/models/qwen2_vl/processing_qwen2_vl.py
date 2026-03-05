a6c2083a33be0588dfb9c8f71d3e357bb54ab13a
tsaxena <tripti.saxena@gmail.com>
2026-03-05 13:30:26 -0800

    Update function signature

--- src/transformers/models/qwen2_vl/processing_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/processing_qwen2_vl.py
@@ -89,6 +89,7 @@ class Qwen2VLProcessor(ProcessorMixin):
         images: Optional[ImageInput] = None,
         text: Union[TextInput, PreTokenizedInput, list[TextInput], list[PreTokenizedInput]] = None,
         videos: Optional[VideoInput] = None,
+        audios: Optional[np.ndarray] = None,
         **kwargs: Unpack[Qwen2VLProcessorKwargs],
     ) -> BatchFeature:
         """
